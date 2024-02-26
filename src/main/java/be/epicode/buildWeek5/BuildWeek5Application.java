e301f200357a4d9d435d4c88e15b3bbc0a4448cc
Giordano <zagattigiordano@hotmail.it>
2024-02-26 10:39:36 +0100

    commit

---
+++ src/main/java/be/epicode/buildWeek5/BuildWeek5Application.java
@@ -0,0 +1,13 @@
+package be.epicode.buildWeek5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildWeek5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildWeek5Application.class, args);
+	}
+
+}
---
+++ src/test/java/be/epicode/buildWeek5/BuildWeek5ApplicationTests.java
@@ -0,0 +1,13 @@
+package be.epicode.buildWeek5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildWeek5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
