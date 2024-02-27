25bec8c534386ba68611e941a1ab52b92f81e789
Cosmin <kosmyn1994@gmail.com>
2024-02-27 11:26:43 +0100

    Controller complete

--- src/main/java/be/epicode/buildWeek5/controllers/AddressController.java
+++ src/main/java/be/epicode/buildWeek5/controllers/AddressController.java
@@ -4,9 +4,11 @@ import be.epicode.buildWeek5.entities.Address;
 import be.epicode.buildWeek5.payloads.AddressDTO;
 import be.epicode.buildWeek5.services.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/address")
@@ -19,6 +21,15 @@ public class AddressController {
     @PostMapping
     public Address saveAddress(@RequestBody AddressDTO payload){return addressService.saveAddress(payload);}
 
+    @PutMapping("/{id}")
+    public Address findByIdAndUpdate(@PathVariable UUID id, @RequestBody AddressDTO updatedAddress){
+        return this.addressService.findByIdAndUpdate(id, updatedAddress );
+    }
 
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void findByIdAndDelete(@PathVariable UUID id){
+        addressService.findByIdAndDelete(id);
+    }
 
 }
