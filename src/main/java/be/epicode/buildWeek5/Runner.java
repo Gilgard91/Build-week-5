6c63ff98d589ed410e5897b6b93ec9176c222252
Giordano <zagattigiordano@hotmail.it>
2024-02-29 17:45:57 +0100

    commit

--- src/main/java/be/epicode/buildWeek5/Runner.java
+++ src/main/java/be/epicode/buildWeek5/Runner.java
@@ -1,8 +1,9 @@
 package be.epicode.buildWeek5;
-
 import be.epicode.buildWeek5.CSV.CSVImporter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,16 +12,49 @@ public class Runner implements CommandLineRunner {
     private CSVImporter csvImporter;
 
     @Autowired
+    private ResourceLoader resourceLoader;
+
+    @Autowired
     public Runner(CSVImporter csvImporter) {
         this.csvImporter = csvImporter;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        String provinceFileName = getClass().getResource("/province-italiane.csv").getFile();
-        String municipalityFileName = getClass().getResource("/comuni-italiani.csv").getFile();
+        Resource province = resourceLoader.getResource("classpath:province-italiane.csv");
+        Resource municipality = resourceLoader.getResource("classpath:comuni-italiani.csv");
+//        String pro = province.getFile().getAbsolutePath();
+//        System.out.println(pro);
+
+        String provinceFileName = province.getFile().getAbsolutePath();
+//                getClass().getResource("/province-italiane.csv").getFile();
+        String municipalityFileName = municipality.getFile().getAbsolutePath();
+//        getClass().getResource("/comuni-italiani.csv").getFile();
 
         csvImporter.importProvinces(provinceFileName);
         csvImporter.importMunicipalities(municipalityFileName);
+
     }
+
 }
+
+
+//@Component
+//public class Runner implements CommandLineRunner {
+//
+//    private CSVImporter csvImporter;
+//
+//    @Autowired
+//    public Runner(CSVImporter csvImporter) {
+//        this.csvImporter = csvImporter;
+//    }
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        String provinceFileName = getClass().getResource("/province-italiane.csv").getFile();
+//        String municipalityFileName = getClass().getResource("/comuni-italiani.csv").getFile();
+//
+//        csvImporter.importProvinces(provinceFileName);
+//        csvImporter.importMunicipalities(municipalityFileName);
+//    }
+//}
